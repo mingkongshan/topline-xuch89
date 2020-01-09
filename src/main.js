f737037749d90555eda7958dc2d01648c7936340
unknown <xuch@xampie.com>
2020-01-09 16:20:09 +0800

    项目初始化-初始化结构-配置和封装axios请求函数

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 执行模块 rem适配:动态设置 html 标签字体大小
+import 'amfe-flexible'
 // 加载注册 Vant 组件模块
 import './utils/register-vant'
+
+// 加载全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 
 // 加载
-import { Button, Cell, CellGroup } from 'vant'
+import {
+  Button,
+  Cell,
+  CellGroup,
+  Icon
+} from 'vant'
 
 // 注册
-Vue.use(Button).use(Cell).use(CellGroup)
+Vue.use(Button)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Icon)
