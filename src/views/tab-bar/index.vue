a08072f4b6b5ec8d515df6aae2fa0c6a18f1ee12
unknown <xuch@xampie.com>
2020-01-16 14:44:51 +0800

    首页-文章列表

--- src/views/tab-bar/index.vue
+++ src/views/tab-bar/index.vue
@@ -5,16 +5,16 @@
     <!-- /子路由出口 -->
 
     <!-- tab-bar 标签栏 -->
-    <van-tabbar route>
-      <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
+
+      <!-- <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="comment-o" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
        <van-tabbar-item
         icon="user-o"
         to="/my"
         >{{ $store.state.user ? '我的' : '未登录' }}
-      </van-tabbar-item>
-    </van-tabbar>
+      </van-tabbar-item> -->
+
     <!-- /tab-bar 标签栏 -->
   </div>
 </template>
