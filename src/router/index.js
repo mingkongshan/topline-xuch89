1c965c3ed6a76779ca4eaaa5f8e4ac8060161b26
unknown <xuch@xampie.com>
2020-02-04 11:24:54 +0800

    文章搜索-创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,11 @@ const routes = [
         path: 'my',
         name: 'my',
         component: () => import('@/views/my')
+      },
+      {
+        path: '/search',
+        name: 'search',
+        component: () => import('@/views/search')
       }
 
     ]
